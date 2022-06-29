e57fa84d64acfb57b09e344d58b7e483962b491f
MatheusCavalini <matheuscavalini@proderj.rj.gov.br>
2022-06-29 14:37:00 -0300

    1- Criação do Projeto 2- Limpeza do Projeto 3- Config Firebase 4- react-router-dom

---
+++ src/pages/about/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+//CSS
+import './About.module.css'
+
+function About(){
+    return(
+        <div>
+            <h1>About</h1>
+        </div>
+    )
+}
+export default About;
