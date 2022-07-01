71c2c5654a4a1687c23013777b50af4c92430905
matheuscav20@gmail.com <matheuscav20@gmail.com>
2022-06-30 21:00:24 -0300

    Css navbar e footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import styles from './Footer.module.css'
 
 const Footer = () =>{
-    return <footer>
+    return <footer className={styles.footer}>
         <h3>Escreva o que você tem interesse!</h3>
         <p>Mini Blog &copy; 2022</p>
     </footer>
