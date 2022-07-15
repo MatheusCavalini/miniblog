35e8bd54e1f6f2b59657ac5aace095e28f25836a
matheuscav20@gmail.com <matheuscav20@gmail.com>
2022-07-15 09:38:10 -0300

    Create login and registration form

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Home from './pages/home/Home';
 import About from './pages/about/About';
 import NavBar from './components/Navbar';
 import Footer from './components/Footer';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
 
 
 function App() {
@@ -18,6 +20,8 @@ function App() {
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
+            <Route path='/login' element={<Login/>}/>
+            <Route path='/register' element={<Register/>}/>
           </Routes>
         </div>
       <Footer/>
